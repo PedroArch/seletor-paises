e0024e2d02c51dd81019516c3c463f317ec42b01
Pedro Carvalho <pedrofrancocarvalho@gmail.com>
2020-10-09 23:24:37 -0300

    Upload v1.0

--- js/script.js
+++ js/script.js
@@ -9,37 +9,53 @@ let totalFavorites = 0;
 
 let numberFormat = null;
 
-window.addEventListener('load', () => {
-  const countriesTab = document.querySelector('.countriesTab');
-  const favoritesTab = document.querySelector('.favoritesTab');
+let countriesTab = null;
+let favoritesTab = null;
+let totalCountriesSpan = null;
+let totalFavoritesSpan = null;
 
-  const totalCountriesSpan = document.querySelector('.totalCountriesSpan');
-  const totalFavoritesSpan = document.querySelector('.totalFavoritesSpan');
+let totalPopulationSpan = null;
+let totalFavoritesPopulationSpan = null;
 
-  const totalPopulationSpan = document.querySelector('.totalPopulationSpan');
-  const totalFavoritesPopulationSpan = document.querySelector(
-    '.totalFavoritesPopulationSpan'
+let countriesButtons = null;
+let favoriteButtons = null;
+
+window.addEventListener("load", () => {
+  countriesTab = document.querySelector(".countriesTab");
+  favoritesTab = document.querySelector(".favoritesTab");
+
+  totalCountriesSpan = document.querySelector(".totalCountriesSpan");
+  totalFavoritesSpan = document.querySelector(".totalFavoritesSpan");
+
+  totalPopulationSpan = document.querySelector(".totalPopulationSpan");
+  totalFavoritesPopulationSpan = document.querySelector(
+    ".totalFavoritesPopulationSpan"
   );
 
-  numberFormat = Intl.NumberFormat('pt-BR');
+  numberFormat = Intl.NumberFormat("pt-BR");
 
   fetchCountries();
 });
 
 async function fetchCountries() {
-  const res = await fetch('https://restcountries.eu/rest/v2/all');
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
   const json = await res.json();
 
-  allCountries = json.map((country) => {
-    const { translations, numericCode, flag, population } = country;
+  allCountries = json
+    .map((country) => {
+      const { translations, numericCode, flag, population } = country;
 
-    return {
-      name: translations.br,
-      id: numericCode,
-      flag,
-      population,
-    };
-  });
+      return {
+        name: translations.br,
+        id: numericCode,
+        flag,
+        population,
+      };
+    })
+    .sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+
   render();
 }
 
@@ -50,7 +66,118 @@ function render() {
   handleCountriesButtons();
 }
 
-function renderCountries() {}
-function renderFavorites() {}
-function renderSummary() {}
-function handleCountriesButtons() {}
+function renderCountries() {
+  let countriesHTML = "<div>";
+
+  allCountries.forEach((country) => {
+    const { name, flag, id, population } = country;
+
+    const countryHTML = `
+    <div class='country'>
+      <div>
+        <a id="${id}"class="waves-effect waves-light btn">+</a>
+      </div>
+      <div>
+        <img src="${flag}" class="flag"  alt="${name}">
+      </div>
+      <div>
+        <ul>
+          <li>${name}</li>
+          <li>${formatNumber(population)}</li>
+        </ul>
+      </div>
+    </div>
+    
+    `;
+
+    countriesHTML += countryHTML;
+  });
+  countriesHTML += "</div>";
+  countriesTab.innerHTML = countriesHTML;
+}
+function renderFavorites() {
+  let favoritesHTML = "<div>";
+
+  allFavorites.forEach((country) => {
+    const { name, flag, id, population } = country;
+    const favoriteHTML = `
+    <div class='country'>
+      <div>
+        <a id="${id}" class="waves-effect waves-light btn red darken-4">-</a>
+      </div>
+      <div>
+        <img src="${flag}" class="flag"  alt="${name}">
+      </div>
+      <div>
+        <ul>
+          <li>${name}</li>
+          <li>${formatNumber(population)}</li>
+        </ul>
+      </div>
+    </div>
+    
+    `;
+
+    favoritesHTML += favoriteHTML;
+  });
+  favoritesHTML += "</div>";
+  favoritesTab.innerHTML = favoritesHTML;
+}
+
+function renderSummary() {
+  totalCountriesSpan.textContent = allCountries.length;
+  totalFavoritesSpan.textContent = allFavorites.length;
+
+  totalPopulationSpan.textContent = formatNumber(
+    allCountries.reduce((acc, current) => {
+      return acc + current.population;
+    }, 0)
+  );
+
+  totalFavoritesPopulationSpan.textContent = formatNumber(
+    allFavorites.reduce((acc, current) => {
+      return acc + current.population;
+    }, 0)
+  );
+}
+
+function handleCountriesButtons() {
+  const CountryButtons = Array.from(countriesTab.querySelectorAll(".btn"));
+  const FavoriteButtons = Array.from(favoritesTab.querySelectorAll(".btn"));
+
+  CountryButtons.forEach((button) => {
+    button.addEventListener("click", () => addToFavorites(button.id));
+  });
+
+  FavoriteButtons.forEach((button) => {
+    button.addEventListener("click", () => removeFromFavorites(button.id));
+  });
+}
+
+function addToFavorites(id) {
+  const currentCountry = allCountries.find((country) => country.id === id);
+  allFavorites = [...allFavorites, currentCountry];
+  allFavorites.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+
+  allCountries = allCountries.filter((country) => country.id !== id);
+
+  render();
+}
+
+function removeFromFavorites(id) {
+  const currentCountry = allFavorites.find((country) => country.id === id);
+  allCountries = [...allCountries, currentCountry];
+  allCountries.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+
+  allFavorites = allFavorites.filter((country) => country.id !== id);
+
+  render();
+}
+
+function formatNumber(number) {
+  return numberFormat.format(number);
+}
